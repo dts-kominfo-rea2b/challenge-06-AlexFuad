31dd07b32c161ce5f8a569a88e5235dd0ac2bb79
AlexFuad <101112263+AlexFuad@users.noreply.github.com>
2022-06-12 14:38:26 +0700

    Update fungsiCustom.js
    Menambahkan var file1, file2 dan file3

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -7,23 +7,44 @@ let file3 = "./data3.json";
 
 // ! JANGAN DIMODIFIKASI
 let modifyFile1 = (val) => {
-  file1 = val;
+    file1 = val;
 };
 let modifyFile2 = (val) => {
-  file2 = val;
+    file2 = val;
 };
 let modifyFile3 = (val) => {
-  file3 = val;
+    file3 = val;
 };
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+// const bacaData = null;
+
+const bacaData = (fnCallback) => {
+    fs.readFile(file1, "utf8", (err, data) => {
+        if (err) fnCallback(err, null);
+        const { message } = JSON.parse(data);
+        result.push(message.split(" ")[1]);
+        fs.readFile(file2, "utf8", (err, data) => {
+            if (err) fnCallback(err, null);
+            const response = JSON.parse(data);
+            console.log(response[0].message.split(" ")[1]);
+            result.push(response[0].message.split(" ")[1]);
+            fs.readFile(file3, "utf8", (err, data) => {
+                if (err) fnCallback(err, null);
+                const response = JSON.parse(data);
+                console.log(response[0].data.message.split(" ")[1]);
+                result.push(response[0].data.message.split(" ")[1]);
+                fnCallback(null, result);
+            });
+        });
+    });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
-  modifyFile1,
-  modifyFile2,
-  modifyFile3,
-  bacaData,
+    modifyFile1,
+    modifyFile2,
+    modifyFile3,
+    bacaData,
 };
